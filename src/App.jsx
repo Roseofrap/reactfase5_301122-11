adbe6aee6a0ac5e316e513e0c81f4a098ddc2163
Roseofrap <hslorena92@gmail.com>
2023-07-14 20:59:46 -0500

    react
    Despliegue react

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,10 @@ function App() {
       <h1> Fase 5 Operar </h1>
       <h1> Mi proyecto </h1>
       <div className="card">
-       
        <h3> Presentado por: </h3>
+       <h4> Lizeth Lorena Herrera Salamanca  </h4>
 
-       <h4> Lizeth Lorena Herrera Sañlamanca  </h4>
+       <h6> Enlace al proyecto </h6>
        <a href="index2.html">Click aqui para ingresar</a>
       </div>
       <p className="read-the-docs">
